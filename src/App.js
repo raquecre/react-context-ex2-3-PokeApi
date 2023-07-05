4e095d091c5a861703dd9114cece27f79a3f82c1
raquecre <raquecre@gmail.com>
2023-07-05 15:35:55 +0200

    App.js OK

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import PokeContextProvider from './context';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
+      <Header />
+      <PokeContextProvider>
+        <Main />
+      </PokeContextProvider>
     </div>
   );
 }
